90c9ff2fb3c8be5eaf828783e5d86402615ba47c
xcheche <chekwubechukwu23@gmail.com>
2024-07-24 16:39:56 +0100

    added view functionality

--- student_manage/urls.py
+++ student_manage/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.list_student, name="home")]
+urlpatterns = [
+    path("", views.list_student, name="home"),
+   
+               ]
--- student_manage/views.py
+++ student_manage/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Student
 
+
 # Create your views here.
 def list_student(request):
     student = Student.objects.all()
     return render(request, "list_student.html", {"student": student})
+
+
+
